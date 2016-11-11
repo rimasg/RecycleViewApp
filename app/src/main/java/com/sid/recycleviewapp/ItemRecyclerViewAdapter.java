953e29e45ee5dc1577452a3b79ab2c54b5e6e1f0
Rimas G <rimasg@gmail.com>
2016-11-11 20:31:15 +0200

    Updates

--- app/src/main/java/com/sid/recycleviewapp/ItemRecyclerViewAdapter.java
+++ app/src/main/java/com/sid/recycleviewapp/ItemRecyclerViewAdapter.java
@@ -1,20 +1,18 @@
 package com.sid.recycleviewapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sid.recycleviewapp.ItemFragment.OnListFragmentInteractionListener;
+import com.sid.recycleviewapp.databinding.FragmentItemBinding;
 import com.sid.recycleviewapp.dummy.EmptyContent.DummyItem;
 
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
- * specified {@link OnListFragmentInteractionListener}.
- * TODO: Replace the implementation with code for your data type.
- */
-public class ItemRecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
+public class ItemRecyclerViewAdapter extends RecyclerView.Adapter<ItemRecyclerViewAdapter.ViewHolder> {
 
     private final List<DummyItem> mValues;
     private final OnListFragmentInteractionListener mListener;
@@ -26,23 +24,19 @@ public class ItemRecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        final ViewHolder viewHolder = ViewHolder.create(parent, R.layout.fragment_item);
+        final ViewHolder viewHolder = ViewHolder.create(parent);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         final DummyItem item = mValues.get(position);
-        holder.binding.setVariable(com.sid.recycleviewapp.BR.data, item);
-        final View contentView = holder.itemView.findViewById(R.id.content);
-        final View viewById = holder.itemView.findViewById(R.id.list_item);
-        viewById.setOnClickListener(new View.OnClickListener() {
+        holder.binding.setData(item);
+        holder.binding.listItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(item, contentView);
+                    mListener.onListFragmentInteraction(item, holder.binding.content);
                 }
             }
         });
@@ -51,5 +45,20 @@ public class ItemRecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public int getItemCount() {
         return mValues.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public FragmentItemBinding binding;
+
+        public ViewHolder(FragmentItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public static ViewHolder create(ViewGroup parent) {
+            FragmentItemBinding binding = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.getContext()), R.layout.fragment_item, parent, false);
+            return new ViewHolder(binding);
+        }
     }
 }
