1a0b540ae20353a026c512324d517d0d111a68a5
Rimas G <rimasg@gmail.com>
2016-12-14 12:01:39 +0200

    Updates

--- app/src/main/java/com/sid/recycleviewapp/DetailActivity.java
+++ app/src/main/java/com/sid/recycleviewapp/DetailActivity.java
@@ -3,6 +3,7 @@ package com.sid.recycleviewapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import static android.R.attr.radius;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String VIEW_DETAILED_TEXT = "detail:header:text";
@@ -29,14 +32,7 @@ public class DetailActivity extends AppCompatActivity {
                 final int cx = detailedImageView.getWidth() / 2;
                 final int cy = detailedImageView.getHeight() / 2;
                 final float radius = (float) Math.hypot(cx, cy);
-                final Animator anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, radius, 0);
-                anim.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        v.setVisibility(View.INVISIBLE);
-                    }
-                });
-                anim.start();
+                createCircularReveal(v, cx, cy);
             }
         });
 
@@ -48,5 +44,18 @@ public class DetailActivity extends AppCompatActivity {
     private void loadData(Intent intent) {
         final String dataString = intent.getStringExtra(VIEW_DETAILED_TEXT);
         detailedText.setText(dataString);
+    }
+
+    private void createCircularReveal(final View v, int x, int y) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            final Animator anim = ViewAnimationUtils.createCircularReveal(v, x, y, radius, 0);
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    v.setVisibility(View.INVISIBLE);
+                }
+            });
+            anim.start();
+        }
     }
 }
